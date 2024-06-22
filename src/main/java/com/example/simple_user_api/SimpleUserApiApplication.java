4b77564d9b61c2cf7bd8f9912bd7879b39ed4376
pipzgutz <pmgutierrez@pm.me>
2024-06-22 14:39:50 +0800

    init

---
+++ src/main/java/com/example/simple_user_api/SimpleUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_user_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUserApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleUserApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/simple_user_api/SimpleUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleUserApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
